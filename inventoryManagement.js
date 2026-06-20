06ca7a94714d64b74fee916c998c47cfb899c301
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-06-20 17:37:10 +0300

    funtions added and tests passed

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,31 @@
 // Write your code here
 
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+function updateProductName(index, productName) {
+  products[index] = productName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
+  logFirstProduct:
+    typeof logFirstProduct !== "undefined" ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== "undefined" ? addProduct : undefined,
+  updateProductName:
+    typeof updateProductName !== "undefined" ? updateProductName : undefined,
+  removeLastProduct:
+    typeof removeLastProduct !== "undefined" ? removeLastProduct : undefined,
+  products,
 };
